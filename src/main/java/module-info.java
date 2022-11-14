ac9a70e351318232cab780d55fdf7f7f4eba66f1
acx365 <sandra.koenig@haw-hamburg.de>
2022-11-14 10:00:03 +0100

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module vsp.trongame {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens vsp.trongame to javafx.fxml;
+    exports vsp.trongame;
+}
