81ffc512cfc49cff3f5c2a00cef92d53b1849b36
acx805 <inken.dulige@haw-hamburg.de>
2022-11-30 20:35:18 +0100

    added component Controller

---
+++ src/main/java/vsp/trongame/app/controller/ITronController.java
@@ -0,0 +1,36 @@
+package vsp.trongame.app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import vsp.trongame.app.model.gamemanager.ITronModel;
+
+/**
+ * The controller which communicates with the model and the view.
+ */
+public interface ITronController {
+
+    /**
+     *
+     * @param scene
+     */
+    void initKeyEventHandler(Scene scene);
+
+    /**
+     * When the Button "spiel starten" is clicked, the controller receives an event from the view and notifies the model.
+     * @param event that the game was started
+     */
+    void startGame(ActionEvent event);
+
+    /**
+     * The keyboard input is received from the view and send to the model.
+     * @param key which was pressed on the keyboard
+     */
+    void handleKeyEvent(KeyEvent key);
+
+    /**
+     * Gets the TronModel which works with TronController.
+     * @return the TronModel which works with the controller
+     */
+    ITronModel getModel();
+}
---
+++ src/main/java/vsp/trongame/app/controller/TronController.java
@@ -0,0 +1,34 @@
+package vsp.trongame.app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import vsp.trongame.app.model.gamemanager.ITronModel;
+
+/**
+ * Implements the interface ITronController
+ */
+public class TronController implements ITronController {
+
+    private ITronModel model;
+
+    @Override
+    public void initKeyEventHandler(Scene scene) {
+
+    }
+
+    @Override
+    public void startGame(ActionEvent event){
+
+    }
+
+    @Override
+    public void handleKeyEvent(KeyEvent event){
+
+    }
+
+    @Override
+    public ITronModel getModel(){
+        return null;
+    }
+}
