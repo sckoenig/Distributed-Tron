83070b5ab7b48b5f5f8aaaa9503358cc67a4d17d
acx805 <inken.dulige@haw-hamburg.de>
2022-12-19 22:37:32 +0100

    add config

--- src/main/java/vsp/trongame/app/model/gamemanagement/Config.java
+++ src/main/java/vsp/trongame/app/model/gamemanagement/Config.java
@@ -15,7 +15,26 @@ import java.util.Properties;
  */
 public class Config {
 
-    public static final Map<String, String> DEFAULTS = new HashMap<>();
+    public static final Map<String, String> DEFAULTS;
+    static{
+        DEFAULTS = new HashMap<>();
+        DEFAULTS.put("width","750");
+        DEFAULTS.put("height", "60");
+        DEFAULTS.put("rows", "30");
+        DEFAULTS.put("columns", "50");
+        DEFAULTS.put("waitingTimer", "5000");
+        DEFAULTS.put("endingTimer", "5000");
+        DEFAULTS.put("defaultPlayerNumber", "2");
+        DEFAULTS.put("speed", "50");
+        DEFAULTS.put("p1", "Q,W");
+        DEFAULTS.put("p2", "C,V");
+        DEFAULTS.put("p3", "D,F");
+        DEFAULTS.put("p4", "O,P");
+        DEFAULTS.put("p5", "M,N");
+        DEFAULTS.put("p6", "Z,U");
+        DEFAULTS.put("gameMode", "LOCAL");
+        DEFAULTS.put("nameServer", "127.0.0.1:5555");
+    }
     private Properties properties;
     private Map<KeyCode, Steer> keyMappings;
     private String filePath = "tronConfig.properties";
@@ -25,7 +44,11 @@ public class Config {
         this.keyMappings = new HashMap<>();
         loadConfigFromFile();
         if(!isConfigValid()){
-            reloadConfig();
+            try {
+                properties = reloadConfig();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
         setKeyMappings();
     }
@@ -35,52 +58,64 @@ public class Config {
      */
     private boolean isConfigValid(){
 
-        if(!properties.containsKey("width")){
-            System.out.println("width");
+        if(!properties.containsKey("width") || !isNumber(properties.getProperty("width"))){
             return false;
-        } else if (!properties.containsKey("height")) {
-            System.out.println("height");
+        } else if (!properties.containsKey("height") || !isNumber((properties.getProperty("height")))) {
             return false;
-        } else if (!properties.containsKey("rows") || Integer.parseInt(properties.getProperty("rows")) < 10) {
-            System.out.println("rows");
+        } else if (!properties.containsKey("rows") || !isNumber(properties.getProperty("rows")) ||
+                Integer.parseInt(properties.getProperty("rows")) < 10) {
             return false;
-        } else if (!properties.containsKey("columns") || Integer.parseInt(properties.getProperty("columns")) < 1 ) {
-            System.out.println("columns");
+        } else if (!properties.containsKey("columns") || !isNumber(properties.getProperty("columns")) ||
+                Integer.parseInt(properties.getProperty("columns")) < 1) {
             return false;
-        } else if (!properties.containsKey("defaultPlayerNumber") || 2 > Integer.parseInt(properties.getProperty("defaultPlayerNumber"))
-                || Integer.parseInt(properties.getProperty("defaultPlayerNumber")) > 6 ) {
-            System.out.println("defaultPlayerNumber");
+        } else if (!properties.containsKey("waitingTimer") || !isNumber(properties.getProperty("waitingTimer"))) {
+
             return false;
-        } else if (1 > Integer.parseInt(properties.getProperty("speed")) || Integer.parseInt(properties.getProperty("speed")) > 100 || !properties.containsKey("speed")){
-            System.out.println("speed");
+        } else if (!properties.containsKey("endingTimer") || !isNumber(properties.getProperty("endingTimer"))) {
+            return false;
+        } else if (!properties.containsKey("defaultPlayerNumber") || !isNumber(properties.getProperty("defaultPlayerNumber"))
+                    || 2 > Integer.parseInt(properties.getProperty("defaultPlayerNumber"))
+                    || Integer.parseInt(properties.getProperty("defaultPlayerNumber")) > 6) {
+            return false;
+        } else if (!properties.containsKey("speed") || !isNumber(properties.getProperty("speed")) ||
+                    1 > Integer.parseInt(properties.getProperty("speed")) || Integer.parseInt(properties.getProperty("speed")) > 100){
             return false;
         }
         else if (!properties.containsKey("p1") || isSteerValid(properties.getProperty("p1"))) {
-            System.out.println("p1");
             return false;
         } else if (!properties.containsKey("p2") || isSteerValid(properties.getProperty("p2"))) {
-            System.out.println("p2");
             return false;
         } else if (!properties.containsKey("p3") || isSteerValid(properties.getProperty("p3"))) {
-            System.out.println("p3");
             return false;
         } else if (!properties.containsKey("p4") || isSteerValid(properties.getProperty("p4"))) {
-            System.out.println("p4");
             return false;
         } else if (!properties.containsKey("p5") || isSteerValid(properties.getProperty("p5"))) {
-            System.out.println("p5");
             return false;
         } else if (!properties.containsKey("p6") || isSteerValid(properties.getProperty("p6"))) {
-            System.out.println("p6");
             return false;
         } else if(!properties.containsKey("gameMode") ||
                 properties.getProperty("gameMode").equalsIgnoreCase(GameModus.LOCAL.name())){
             if (properties.getProperty("gameMode").equalsIgnoreCase(GameModus.NETWORK.name())) {
-                System.out.println("gameMode");
                 return false;
             }
         } else if (!properties.containsKey("nameServer") || properties.getProperty("nameServer").equalsIgnoreCase("127.0.0.1:5555")) {
-            System.out.println("nameServer");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if the given String is numeric.
+     * @param string which should be a number.
+     * @return if the string is a number
+     */
+    private boolean isNumber(String string){
+        if (string == null) {
+            return false;
+        }
+        try {
+             Integer.parseInt(string);
+        } catch (NumberFormatException nfe) {
             return false;
         }
         return true;
@@ -106,9 +141,7 @@ public class Config {
                 tempStringArray[1].equalsIgnoreCase("Left") ||
                 tempStringArray[1].equalsIgnoreCase("Up") ||
                 tempStringArray[1].equalsIgnoreCase("Down"))){
-            if(!tempStringArray[1].matches(regex)){
-                return false;
-            }
+            return tempStringArray[1].matches(regex);
         }
         return true;
     }
@@ -117,9 +150,32 @@ public class Config {
      * Reloads the config file from the system.
      * @return the new properties file
      */
-    private Properties reloadConfig(){
-        System.out.println("geht noch nicht");
-        return null;
+    private Properties reloadConfig() throws IOException {
+
+        OutputStream outputStream = new FileOutputStream(filePath);
+
+        Properties prop = new Properties();
+
+        prop.setProperty("width", DEFAULTS.get("width"));
+        prop.setProperty("height", DEFAULTS.get("height"));
+        prop.setProperty("rows", DEFAULTS.get("rows"));
+        prop.setProperty("columns", DEFAULTS.get("columns"));
+        prop.setProperty("waitingTimer", DEFAULTS.get("waitingTimer"));
+        prop.setProperty("endingTimer", DEFAULTS.get("endingTimer"));
+        prop.setProperty("defaultPlayerNumber", DEFAULTS.get("defaultPlayerNumber"));
+        prop.setProperty("speed", DEFAULTS.get("speed"));
+        prop.setProperty("p1", DEFAULTS.get("p1"));
+        prop.setProperty("p2", DEFAULTS.get("p2"));
+        prop.setProperty("p3", DEFAULTS.get("p3"));
+        prop.setProperty("p4", DEFAULTS.get("p4"));
+        prop.setProperty("p5", DEFAULTS.get("p5"));
+        prop.setProperty("p6", DEFAULTS.get("p6"));
+        prop.setProperty("gameMode", DEFAULTS.get("gameMode"));
+        prop.setProperty("nameServer", DEFAULTS.get("nameServer"));
+        prop.store(outputStream, null);
+
+        outputStream.close();
+        return prop;
     }
 
     /**
@@ -130,9 +186,8 @@ public class Config {
         for (int i = 1; i < 7; i++) {
             keys = properties.getProperty("p"+i);
             String[] temp = keys.split(",");
-            keyMappings.put(KeyCode.valueOf(temp[0]), new Steer(i, DirectionChange.LEFT));
-                    //PROBLEM steer braucht Direction wir haben aber nur DirectionChange
-            keyMappings.put(KeyCode.valueOf(temp[1]), new Steer(i, DirectionChange.RIGHT));
+            keyMappings.put(KeyCode.valueOf(temp[0]), new Steer(i, DirectionChange.LEFT_STEER));
+            keyMappings.put(KeyCode.valueOf(temp[1]), new Steer(i, DirectionChange.RIGHT_STEER));
         }
 
     }
@@ -146,7 +201,11 @@ public class Config {
         try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(propertiesFile))){
             properties.load(bis);
         }catch (IOException e){
-            properties = reloadConfig();
+            try {
+                properties = reloadConfig();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 
