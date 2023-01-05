544e23c06175c9eb8e8cd92f5584c4709e2aceb3
acx805 <inken.dulige@haw-hamburg.de>
2023-01-05 21:48:05 +0100

    added comments

--- src/main/java/vsp/middleware/serverstub/IUnmarshaller.java
+++ src/main/java/vsp/middleware/serverstub/IUnmarshaller.java
@@ -2,7 +2,17 @@ package vsp.middleware.serverstub;
 
 public interface IUnmarshaller {
 
+    /**
+     * Adds a message to a queue.
+     *
+     * @param message which is added
+     */
     void addToQueue(byte[] message);
 
+    /**
+     * Sets the given Port.
+     *
+     * @param port which is set
+     */
     void setPort(int port);
 }
